bd363eee9e7251e960b92046118c4c0f58030d9d
Long Thoi <longdk251@gmail.com>
2021-10-30 02:05:48 +0700

    update fe

---
+++ src/layout/LayoutClient.vue
@@ -0,0 +1,9 @@
+<template>
+  <header><h1>Header-Client</h1></header>
+  <router-view />
+  <footer><h1>Footer-Client</h1></footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ src/views/error/Error404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 >
+      Sorry, nothing here...<strong>(404)</strong>
+    </h2>
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Error404'
+}
+</script>
