d281f8799d5b57b31c045d646a8ae966483003da
ddthien-coder <jungjihoonkr97@gmail.com>
2021-10-30 14:50:51 +0700

    update

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,10 +1,14 @@
 const routes = [
   {
     path: "/",
-    component: () => import("@/layout/LayoutClient.vue"),
+    component: () => import("@/layout/client/Layout.vue"),
     children: [
         { path: "/", component: () => import("@/views/client/Home.vue") },
     ],
+  },
+  {
+    path: "/digital/admin",
+    component: () => import("@/layout/admin/Layout.vue")
   },
 ];
 // Always leave this as last one
@@ -12,7 +16,7 @@ if (process.env.MODE !== 'ssr') {
   routes.push({
       path: '/:catchAll(.*)',
       component: () =>
-          import ('../views/error/Error404.vue')
+          import ('@/views/error/Error404.vue')
   })
 }
 export default routes;
