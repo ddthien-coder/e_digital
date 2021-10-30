c6665bf88892bb449c68d39ee2f377e654508281
ddthien-coder <jungjihoonkr97@gmail.com>
2021-10-30 14:28:36 +0700

    update

--- src/layout/LayoutClient.vue
+++ src/layout/LayoutClient.vue
@@ -1,9 +1,39 @@
 <template>
-  <header><h1>Header-Client</h1></header>
-  <router-view />
-  <footer><h1>Footer-Client</h1></footer>
+     <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
 </template>
 <script>
 export default {};
 </script>
-<style></style>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 620px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router/index.js'
 import store from './store'
 
+// library ui
+import ElementPlus from 'element-plus';
+
+// change en languages
+import enLang from 'element-plus/lib/locale/lang/en'
+
 createApp(App)
 .use(store)
 .use(router)
+.use(ElementPlus, { locale: enLang})
 .mount('#app')
